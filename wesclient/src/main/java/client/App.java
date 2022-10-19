06b94a07bd0a6d6e0fa462edb5e057a2862ef34b
lai2-01 <lai2-01@itismeucci.com>
2022-10-19 09:29:04 +0200

    correzione primo commit

--- wesclient/src/main/java/client/App.java
+++ wesclient/src/main/java/client/App.java
@@ -1,13 +1,19 @@
 package client;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws Exception {
+        Socket s = new Socket("localhost", 3000);
+        PrintWriter pr = new PrintWriter(s.getOutputStream(), true);
+        BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        BufferedReader tastiera = new BufferedReader(new InputStreamReader(System.in));
+        pr.println("Pronto");
+        System.out.println(br.readLine());
+        pr.println(tastiera.readLine()); 
+        System.out.println(br.readLine()); 
+        s.close();
     }
 }
